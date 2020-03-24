823a57bca15b092e6573d2438b9b22934baa45f9
Alejandro Francisco García Uclés <38355552+AlejandroFrGU@users.noreply.github.com>
2020-03-24 19:26:44 +0100

    Updated to just problem

--- Listados/parameterizeFunctionProblem.java
+++ Listados/parameterizeFunctionProblem.java
@@ -4,7 +4,3 @@ function tenPercentRaise(aPerson) {
 function fivePercentRaise(aPerson) {
   aPerson.salary = aPerson.salary.multiply(1.05);
 }
-
-function raise(aPerson, factor) {
-  aPerson.salary = aPerson.salary.multiply(1 + factor);
-}
