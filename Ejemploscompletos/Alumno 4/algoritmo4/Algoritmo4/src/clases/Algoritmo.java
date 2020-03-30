2792965a5edf2968cee5c48a60e33b3160011c75
unknown <fgl244@inlumine.ual.es>
2020-03-30 17:56:30 +0200

    Algoritmo de actividades

---
+++ Ejemploscompletos/Alumno 4/algoritmo4/Algoritmo4/src/clases/Algoritmo.java
@@ -0,0 +1,26 @@
+package clases;
+
+import java.util.ArrayList;
+
+public class Algoritmo {
+
+	public int algoritmo(ArrayList<Actividad> actividades) {
+		int companeros = 0;
+		if(actividades.size() == 1) {
+			return companeros;
+		}
+		for (int i = 0; i < actividades.size(); i++) {
+			for (int j = i+1; j < actividades.size(); j++) {
+				if(actividades.get(i).getFinalizacion() > actividades.get(j).getComienzo()) {
+					companeros++;
+				} else {
+					i = j;
+					break;
+				}
+				if(j == actividades.size() - 1) return companeros;
+			}
+		}
+		return companeros;
+	}
+	
+}
